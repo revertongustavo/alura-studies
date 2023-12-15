92805bfd758aae52b480fcc3ebb9ed7bdc66dcf4
Reverton Silva <revertong@gmail.com>
2023-12-15 14:44:35 -0700

    Criando interfaces

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,12 +1,38 @@
 import React from 'react'
 import Botao from '../Botao'
 import style from './Formulario.module.scss';
+import { ITarefa } from '../../types/tarefa';
+import {v4 as uuidv4} from 'uuid';
 
 
-class Formulario extends React.Component {
+class Formulario extends React.Component <{
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}> {
+    state ={
+        tarefa: "",
+        tempo: "00:00"
+    }
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>){
+        evento.preventDefault()
+        this.props.setTarefas(tarefasAntigas => 
+            [
+                ...tarefasAntigas, 
+                {
+                    ...this.state,
+                    selecionado: false,
+                    completado: false,
+                    id: uuidv4()
+                }
+            ]
+        )
+        this.setState({
+            tarefa: "",
+            tempo: "00:00:00"
+        })
+    }
  render() {
     return (
-        <form className={style.novaTarefa}>
+        <form className={style.novaTarefa} onSubmit={ this.adicionarTarefa.bind(this)}>
             <div className={style.inputContainer}>
                 <label htmlFor="tarefa">
                     Adicione um novo estudo
@@ -15,6 +41,8 @@ class Formulario extends React.Component {
                     type= "text"
                     name = "tarefa"
                     id= "tarefa"
+                    value = {this.state.tarefa}
+                    onChange={evento => this.setState({...this.state, tarefa: evento.target.value})}
                     placeholder = "o que você quer estudar"
                     required 
                 />
@@ -28,6 +56,10 @@ class Formulario extends React.Component {
                     type = "time"
                     step ="1"
                     name = "tempo"
+                    value = {this.state.tempo}
+                    onChange={evento => {
+                        this.setState({...this.state, tempo: evento.target.value})
+                    }}
                     id= "tempo"
                     min = "00:00:00"
                     max = "01:30:00"
@@ -35,7 +67,7 @@ class Formulario extends React.Component {
                 />
 
             </div>
-            <Botao>Adicionar</Botao>
+            <Botao type = "submit">Adicionar</Botao>
         </form>
     )
  }
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,19 +1,9 @@
-import React from 'react';
 import  style from './Lista.module.scss';
 import Item from './Item';
+import { ITarefa } from '../../types/tarefa';
 
-function Lista() {
-    const tarefas = [{
-        tarefa: 'React',
-        tempo: '02:00:00'
-    }, {
-        tarefa: 'Javascript',
-        tempo: '01:00:00'
-    }, {
-        tarefa: 'Typescript',
-        tempo: '03:00:00'
-    }
-    ]
+
+function Lista({ tarefas } : {tarefas: ITarefa[] }) {
     return (
         <aside className={style.listaTarefas}>
             <h2> Estudos do dia </h2>
